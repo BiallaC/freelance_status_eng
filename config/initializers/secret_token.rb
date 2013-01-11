327b3c8c866aa5d34f7cf07ee87b57a68fa6a293
Christoph Biallas <christoph.biallas@googlemail.com>
2013-01-11 16:54:44 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreelanceStatusEng::Application.config.secret_token = '4e3b23d2d3e516be478a0b924ea4238c0460f59133ab13d27eb326320ea3a9998f0432210d1b5cf91eb6cca87e3c7fbbcdb22cca1583f86444ac68e4b3fc22c7'
